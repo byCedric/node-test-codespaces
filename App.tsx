619d030762b5f55db49ccc50fa72aa961cc90a52
Cedric van Putten <me@bycedric.com>
2021-09-24 17:06:25 +0000

    Add image to test assets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const Icon = require('./assets/icon.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={Icon} style={{ width: 100, height: 100 }} />
+      <Text>Hello from Codespaces!</Text>
       <StatusBar style="auto" />
     </View>
   );
